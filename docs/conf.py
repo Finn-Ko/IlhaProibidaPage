8a2ff2bec6e334fd889feb8faa5f571b4ae2e5b0
Finn Kockelke <Finn_Kockelke@gmx.net>
2023-12-25 22:05:50 -0300

    feat: add autodoc md support

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "sphinx_markdown_builder"]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -33,3 +33,10 @@ language = 'pt'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+def skip(app, what, name, obj,would_skip, options):
+    if name in ( '__init__',):
+        return False
+    return would_skip
+def setup(app):
+    app.connect('autodoc-skip-member', skip)

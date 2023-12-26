e806201113b14ca732e296bada3bd3cff0a16f6a
Finn Kockelke <Finn_Kockelke@gmx.net>
2023-12-25 21:47:55 -0300

    docs: add docs

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../page"))
+sys.path.insert(0, os.path.abspath("..."))
+
+project = 'Ilha Proibida'
+copyright = '2023, Anderson Amorim, Fernanda Araujo, Finn Kockelke, Vanessa M Vianna'
+author = 'Anderson Amorim, Fernanda Araujo, Finn Kockelke, Vanessa M Vianna'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'pt'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/pySrc.rst
@@ -0,0 +1,53 @@
+pySrc package
+=============
+
+Submodules
+----------
+
+pySrc.aventureiro module
+------------------------
+
+.. automodule:: pySrc.aventureiro
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pySrc.canvas module
+-------------------
+
+.. automodule:: pySrc.canvas
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pySrc.ilha module
+-----------------
+
+.. automodule:: pySrc.ilha
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pySrc.jogador module
+--------------------
+
+.. automodule:: pySrc.jogador
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pySrc.main module
+-----------------
+
+.. automodule:: pySrc.main
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+Module contents
+---------------
+
+.. automodule:: pySrc
+   :members:
+   :undoc-members:
+   :show-inheritance:

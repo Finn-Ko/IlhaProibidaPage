44038604d8f5a4322d4259614cbde40330b66486
Finn Kockelke <Finn_Kockelke@gmx.net>
2023-12-26 21:02:56 -0300

    fix: remove previous test

--- page/pySrc/canvas.py
+++ page/pySrc/canvas.py
@@ -24,7 +24,7 @@ class Canvas:
 
         """
         img = window.Image.new()
-        img.src = "img/background.jpg"
+        img.src = island.image_src
 
         def on_image_load(e):
             """
